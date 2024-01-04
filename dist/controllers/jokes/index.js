259d556ccafafbef77d1b5b1cd3dec3eed27bcb3
Jenniina <jenniina@jenniina.fi>
2024-01-04 14:04:37 +0200

    fix undefined id

--- dist/controllers/jokes/index.js
+++ dist/controllers/jokes/index.js
@@ -329,12 +329,12 @@ const updateJoke = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
             //     console.error(EErrorSendingMail[body.language as ELanguage], error)
             //   )
             const subject = 'A joke needs verification';
-            const message = `${body.jokeId}, ${body._id}, ${body.type}, ${body.category}, ${body.language}, ${body.safe}, ${Object.entries(body.flags)
+            const message = `${body.jokeId}, ${findJoke._id}, ${body.type}, ${body.category}, ${body.language}, ${body.safe}, ${Object.entries(body.flags)
                 .filter(([key, value]) => value)
                 .map(([key, value]) => key)
                 .join(', ')}, ${body.user}, ${body.type === types_1.EJokeType.twopart && body.setup ? body.setup : ''}, ${body.type === types_1.EJokeType.twopart && body.delivery ? body.delivery : ''}, ${body.type === types_1.EJokeType.single && body.body ? body.body : ''}`;
             const adminEmail = process.env.NODEMAILER_USER || '';
-            const link = `${process.env.BASE_URI}/api/jokes/${body._id}/verification`;
+            const link = `${process.env.BASE_URI}/api/jokes/${findJoke._id}/verification`;
             const language = (_l = body.language) !== null && _l !== void 0 ? _l : 'en';
             (0, email_1.sendMail)(subject, message, adminEmail, language, link)
                 .then((response) => {
@@ -393,9 +393,7 @@ const deleteUserFromJoke = (req, res) => __awaiter(void 0, void 0, void 0, funct
         });
     }
     catch (error) {
-        res
-            .status(500)
-            .json({
+        res.status(500).json({
             success: false,
             message: EError[req.params.lang] || 'An error occurred',
             error,
@@ -435,9 +433,7 @@ const findJokeByJokeIdLanguageCategoryType = (req, res) => __awaiter(void 0, voi
         res.status(200).json(joke);
     }
     catch (error) {
-        res
-            .status(500)
-            .json({
+        res.status(500).json({
             success: false,
             message: EError[req.params.language] || 'An error occurred',
             error,
@@ -476,9 +472,7 @@ const getJokesByUserAndCategory = (req, res) => __awaiter(void 0, void 0, void 0
         res.status(200).json({ jokes });
     }
     catch (error) {
-        res
-            .status(500)
-            .json({
+        res.status(500).json({
             success: false,
             message: EError[req.params.language] || 'An error occurred',
             error,
@@ -496,9 +490,7 @@ const getJokesByUserAndType = (req, res) => __awaiter(void 0, void 0, void 0, fu
         res.status(200).json({ jokes });
     }
     catch (error) {
-        res
-            .status(500)
-            .json({
+        res.status(500).json({
             success: false,
             message: EError[req.params.language] || 'An error occurred',
             error,
@@ -516,9 +508,7 @@ const getJokesByUserAndSafe = (req, res) => __awaiter(void 0, void 0, void 0, fu
         res.status(200).json({ jokes });
     }
     catch (error) {
-        res
-            .status(500)
-            .json({
+        res.status(500).json({
             success: false,
             message: EError[req.params.language] || 'An error occurred',
             error,
